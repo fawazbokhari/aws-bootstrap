df47ef0b2899845a255db0ef87601e540fb65959
Fawaz Bokhari <fawaz.bokhari@gmail.com>
2020-04-15 10:29:37 +0500

    Add stack name to server output

--- start-service.sh
+++ start-service.sh
@@ -1,4 +1,21 @@
 #!/bin/bash -xe
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app/release
+
+# Query the EC2 metadata service for this instance's region
+
+#REGION=`curl -s http://169.254.169.254/latest/dynamic/instanceidentity/document | jq .region -r`
+
+REGION="`wget -q -O http://169.254.169.254/latest/dynamic/instanceidentity/document | jq .region -r`"
+
+# Query the EC2 metadata service for this instance's instance-id
+
+#export INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instanceid`
+export INSTANCE_ID="`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`"
+
+# Query EC2 describeTags method and pull out the CFN Logical ID for this
+instance
+
+export STACK_NAME=`aws --region $REGION ec2 describe-tags --filters "Name=resource-id,Values=${INSTANCE_ID}" "Name=key,Values=aws:cloudformation:stack-name" | jq -r ".Tags[0].Value"`
+
 npm run start 
